4ce33d322b5bc668b84a01a5e15e82d47b254255
Kyaw-Zayar <81757007+Kyaw-Zayar@users.noreply.github.com>
2023-02-16 12:49:58 +0630

    chg photo

--- resources/views/layouts/cus_5.blade.php
+++ resources/views/layouts/cus_5.blade.php
@@ -377,7 +377,7 @@
           </div>
           <div id="jacket_front" class="content">
             <div class="measure-img-wrapper">
-              <img src="{{asset("assets/images/customize/cus5images/jacket-length.jpg")}}"
+              <img src="{{asset("assets/images/customize/cus5images/jacket-front-length.jpg")}}"
                    alt="jacket_front">
             </div>
             <div class="measure-input-wrapper">
@@ -414,7 +414,7 @@
           </div>
           <div id="vest_length" class="content">
             <div class="measure-img-wrapper">
-              <img src="{{asset("assets/images/customize/cus5images/vest.png")}}"
+              <img src="{{asset("assets/images/customize/cus5images/vest.jpg")}}"
                    alt="vest_length">
             </div>
             <div class="measure-input-wrapper">
@@ -671,7 +671,7 @@
           </div>
           <div id="pants_length" class="content">
             <div class="measure-img-wrapper">
-              <img src="{{asset("assets/images/customize/cus5images/pant-length.png")}}"
+              <img src="{{asset("assets/images/customize/cus5images/pant-length.jpg")}}"
                    alt="pants_length">
             </div>
             <div class="measure-input-wrapper">
